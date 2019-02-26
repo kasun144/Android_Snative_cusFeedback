c056c85aa6e5aa97444a35305b39be017f33a059
kasun144 <kasun144@gmail.com>
2019-02-26 14:02:47 +0530

    button Activity.java's

---
+++ app/src/main/java/com/example/np_fb/ButtName.java
@@ -0,0 +1,20 @@
+package com.example.np_fb;
+
+
+public class ButtName {
+    private String name;
+    private int status;
+
+    public ButtName(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
